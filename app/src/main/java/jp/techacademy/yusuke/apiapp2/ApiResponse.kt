3e581938db3ee158b41e0dba5e4bbdc1245aff8d
ymdysk <ymdysk@sic.shimano.co.jp>
2022-11-10 16:52:29 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/yusuke/apiapp2/ApiResponse.kt
@@ -0,0 +1,31 @@
+package jp.techacademy.yusuke.apiapp2
+
+import com.google.gson.annotations.SerializedName
+
+data class ApiResponse(
+    @SerializedName("results")
+    var results: Results
+)
+
+data class Results(
+    @SerializedName("shop")
+    var shop: List<Shop>
+)
+
+data class Shop(
+    @SerializedName("id")
+    var id: String,
+    @SerializedName("coupon_urls")
+    var couponUrls: CouponUrls,
+    @SerializedName("logo_image")
+    var logoImage: String,
+    @SerializedName("name")
+    var name: String
+)
+
+data class CouponUrls(
+    @SerializedName("pc")
+    var pc: String,
+    @SerializedName("sp")
+    var sp: String
+)
